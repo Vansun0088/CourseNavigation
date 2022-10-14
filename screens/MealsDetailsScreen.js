e50ed3ee1793535499e1e3fd045c0443d3ba53e2
Ivan Gavrylychev <vav-ini@ukr.net>
2022-10-14 18:14:00 +0200

    Base is finished

--- screens/MealsDetailsScreen.js
+++ screens/MealsDetailsScreen.js
@@ -1,36 +1,40 @@
 import { useLayoutEffect, useState } from "react";
-import { Text, View, Image, StyleSheet, ScrollView, Button, Pressable } from "react-native";
+import { Text, View, Image, StyleSheet, ScrollView, Pressable } from "react-native";
 
+import IconButton from "../components/IconButton";
 import List from "../components/MealDetail/List";
 import Subtitle from "../components/MealDetail/Subtitle";
 import Specifics from "../components/Styles/Specifics";
 import { MEALS } from "../data/dummy-data";
 
 function MealsDetailsScreen({ route, navigation }) {
-  const [headerImage, setHeaderImage] = useState(require("../assets/images/whiteStar.png"));
+  // const [headerImage, setHeaderImage] = useState(require("../assets/images/whiteStar.png"));
+  const [imageColor, setImageColor] = useState("white");
 
   const id = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === id);
 
   function headerButtonPressHandler() {
-    setHeaderImage(require("../assets/images/goldStar.png"));
+    //  setHeaderImage(require("../assets/images/goldStar.png"));
+    setImageColor("yellow");
   }
 
   useLayoutEffect(() => {
     navigation.setOptions({
       title: selectedMeal.title,
       headerRight: () => {
-        return (
-          <Pressable onPress={headerButtonPressHandler}>
+        /* return (
+          <Pressable style={styles.outerHeaderImageContainer} onPress={headerButtonPressHandler}>
             <View style={styles.headerImageContainer}>
               <Image style={styles.headerImage} source={headerImage} />
             </View>
           </Pressable>
-        );
+        );*/
+        return <IconButton icon="star" color={imageColor} onPress={headerButtonPressHandler} />;
       },
     });
-  }, [navigation, headerImage]);
+  }, [navigation, headerButtonPressHandler]);
 
   return (
     <ScrollView style={styles.rootContainer}>
@@ -60,9 +64,13 @@ const styles = StyleSheet.create({
   rootContainer: {
     marginBottom: 32,
   },
+  outerHeaderImageContainer: {
+    padding: 10,
+  },
   headerImageContainer: {
     width: 20,
     height: 20,
+    padding: 0,
   },
   headerImage: {
     width: "100%",
